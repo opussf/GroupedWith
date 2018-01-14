ff100ba048c567fffe963903c0677eddd40aa80a
opussf <github@zz9-za.com>
2018-01-14 03:40:21 -0800

    Changed boiler plate stuff.  Some debug

--- src/GroupedWith.lua
+++ src/GroupedWith.lua
@@ -1,5 +1,5 @@
-HITLIST_MSG_VERSION = GetAddOnMetadata("GroupedWith","version")
-HITLIST_MSG_ADDONNAME = "GroupedWith"
+GROUPEDWITH_MSG_VERSION = GetAddOnMetadata("GroupedWith","version")
+GROUPEDWITH_MSG_ADDONNAME = "GroupedWith"
 
 -- Colours
 COLOR_RED = "|cffff0000"
@@ -20,15 +20,23 @@ GroupedWith = {}
 -- loaded
 function GroupedWith.OnLoad()
 	GroupedWithFrame:RegisterEvent("ADDON_LOADED")
+	GroupedWithFrame:RegisterEvent("PARTY_MEMBERS_CHANGED")
 
 	--register slash commands
-	SLASH_HITLIST1 = "/hitlist";
-	SLASH_HITLIST2 = "/hl";
-	SlashCmdList["HITLIST"] = function(msg) Hitlist.command(msg); end
+	SLASH_GROUPEDWITH1 = "/groupedwith";
+	SLASH_GROUPEDWITH2 = "/gw";
+	SlashCmdList["GROUPEDWITH"] = function(msg) GroupedWith.command(msg); end
 
 	-- Chat system hook
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", Hitlist.CHAT_MSG_SYSTEM)
-	GameTooltip:HookScript( "OnTooltipSetUnit", Hitlist.HookSetUnit )
+--	ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", Hitlist.CHAT_MSG_SYSTEM)
+--	GameTooltip:HookScript( "OnTooltipSetUnit", Hitlist.HookSetUnit )
 end
 
 -- events
+function GroupedWith.ADDON_LOADED()
+end
+
+function GroupedWith.PARTY_MEMBERS_CHANGED()
+	print( "PARTY_MEMBERS_CHANGED" )
+end
+
